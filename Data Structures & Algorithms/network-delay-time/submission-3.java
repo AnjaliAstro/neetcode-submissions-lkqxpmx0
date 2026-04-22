3e03746c32424df7027cf0cb92ae797fcb2b6f69
Anjali <31510105+AnjaliAstro@users.noreply.github.com>
2026-04-22 21:20:53 +0530

    Add: network-delay-time - submission-3

---
+++ Data Structures & Algorithms/network-delay-time/submission-3.java
@@ -0,0 +1,49 @@
+class Solution {
+    Map<Integer, Map<Integer, Integer>> adj;
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int[] nodeTime = new int[n+1];
+        Arrays.fill(nodeTime, -1);
+        nodeTime[k] = 0;
+
+        adj = new HashMap<>();
+
+        for(int[] time:times){ //O(E)
+            Map<Integer, Integer> map = adj.getOrDefault(time[0], new HashMap<>());
+            map.put(time[1], time[2]);
+            adj.put(time[0], map);
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(a[1]-b[1]));
+        pq.add(new int[]{k, 0});
+
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int currNode = curr[0];
+            int currWeight = curr[1];
+            Map<Integer, Integer> map = adj.getOrDefault(currNode, new HashMap<>());
+
+            for(Integer key: map.keySet()){ //O(V)
+                if(nodeTime[key] == -1 || nodeTime[key] > map.get(key) + nodeTime[currNode]){
+                    nodeTime[key] = map.get(key) + nodeTime[currNode];
+                    pq.add(new int[]{key, nodeTime[key] }); //O(ElogE)
+                }
+            }// O((V+E)logE)
+        }
+        
+        int minTime = Integer.MIN_VALUE;
+        for(int i=1; i<n+1; i++){
+            if(nodeTime[i] == -1){
+                return -1;
+            }
+            minTime = Math.max(minTime, nodeTime[i]);
+        }
+        return minTime;
+    }
+}
+
+//Time :-
+//  Total: O((V + E) log E)
+// Since E ≤ V², log E ≤ 2 log V, equivalently O((V + E) log V)
+
+// Space :-
+// O(V + E)
