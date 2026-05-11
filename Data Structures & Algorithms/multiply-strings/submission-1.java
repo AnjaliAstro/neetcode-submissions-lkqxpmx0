bee510a7b8646c46cb5e1d9100f13e54626d1778
Anjali <31510105+AnjaliAstro@users.noreply.github.com>
2026-05-11 18:11:08 +0530

    Add: multiply-strings - submission-1

---
+++ Data Structures & Algorithms/multiply-strings/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        if(num1.equals("0") || num2.equals("0")) return "0";
+        
+        int[] result = new int[num1.length() + num2.length()];
+
+        for(int i = num1.length()-1; i >= 0; i--){
+            for(int j = num2.length()-1; j >= 0; j--){
+                int mult = (num1.charAt(i)-'0') * (num2.charAt(j)-'0');
+                int p1 = i+j, p2 = i+j+1;
+                int total = mult + result[p2];
+                result[p2] = total % 10;
+                result[p1] += total / 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int digit : result){
+            if(sb.length() == 0 && digit == 0) continue; // skip leading zeros
+            sb.append(digit);
+        }
+        return sb.toString();
+    }
+}
